72a20727c6aaf705316244e4df1200b641f47147
MousGamee <moussa.sagna@hotmail.fr>
2020-12-16 17:32:39 +0100

    finir les card et faire la page de connexion

--- src/pages/Favoris.jsx
+++ src/pages/Favoris.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import MobileNavbar from '../components/MobileNavbar'
+import Navbar from '../components/Navbar'
+import { Link } from 'react-router-dom'
 
 const Favoris = () => {
     return (
         <>
-            Favoris
-            <MobileNavbar />
+         <MobileNavbar />
+         <Navbar />
+          <div className="mt-4 container-fluid">
+              <div className="d-flex flex-column">
+                <h2>Enregistrés</h2>
+                <Link to='/Connection'><button className="btn btn-secondary mt-2">Connexion</button></Link>
+                <hr/>
+              </div>            
+          </div>
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,12 +18,12 @@ const Home = () => {
         <div className="currentClass">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-6 col-md-6 col-lg-4 classCard">
+                    <div className="col-12 col-md-6 col-lg-4 classCard shadow rounded">
                         <img src="img/image1.jpg" className="img-fluid" alt=""/>
                         <div className="description">
                             <h3 className="title">Ma classe</h3>
                             <span className="h5">Teacher</span>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio sed dignissimos placeat, voluptatibus iure in consequuntur quae architecto maiores cumque odio quibusdam debitis? Qui nesciunt excepturi cum repudiandae, saepe iure.</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio sed digvoluptatibus iure in consequuntur quae architecto maiores cumque odio quibusdam debitis? Qui nesciunt excepturi cum repudiandae, saepe iure.</p>
                         </div>
                     </div>
                 </div>
