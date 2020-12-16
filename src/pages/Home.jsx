b7dbbc9fce6467875f2a06b32fd8d1058e5f944b
MousGamee <moussa.sagna@hotmail.fr>
2020-12-16 17:03:49 +0100

    faire la card pour lacceuill

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,26 +12,19 @@ const Home = () => {
          <Navbar />
          <MobileSearchbar />
 
-        <div className="landing" style={{backgroundImage : `url(${bkg})`, backgroundPosition : 'center'}}>
+        <div className="landing mb-4" style={{backgroundImage : `url(${bkg})`, backgroundPosition : 'center'}}>
         </div>
 
         <div className="currentClass">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-3 classcard">
+                    <div className="col-6 col-md-6 col-lg-4 classCard">
                         <img src="img/image1.jpg" className="img-fluid" alt=""/>
-                    </div>
-                    <div className="col-md-3 classcard">
-                        <img src="img/image1.jpg" className="img-fluid" alt=""/>
-                    </div>
-                    <div className="col-md-3 classcard">
-                        <img src="img/image1.jpg" className="img-fluid" alt=""/>
-                    </div>
-                    <div className="col-md-3 classcard">
-                        <img src="img/image1.jpg" className="img-fluid" alt=""/>
-                    </div>
-                    <div className="col-md-3 classcard">
-                        <img src="img/image1.jpg" className="img-fluid" alt=""/>
+                        <div className="description">
+                            <h3 className="title">Ma classe</h3>
+                            <span className="h5">Teacher</span>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio sed dignissimos placeat, voluptatibus iure in consequuntur quae architecto maiores cumque odio quibusdam debitis? Qui nesciunt excepturi cum repudiandae, saepe iure.</p>
+                        </div>
                     </div>
                 </div>
             </div>
